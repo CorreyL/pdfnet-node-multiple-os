fe8ef49818d1ac6e7aa760c35457d99951196a6a
Correy Lim <correylim@gmail.com>
2022-03-07 15:06:12 -0800

    Add logic to use SQLite3 binary

--- main.js
+++ main.js
@@ -2,6 +2,28 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+const sqlite3 = require('sqlite3').verbose();
+
+// Initialize SQLite 3 Database in-memory, create entries and print them
+// to demonstrate it works
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+    console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
