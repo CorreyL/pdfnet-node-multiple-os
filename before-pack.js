e43bd1c1c9127943500683aad8cceeaffb185268
Correy Lim <correylim@gmail.com>
2022-03-07 15:48:47 -0800

    Reduce code repetition

--- before-pack.js
+++ before-pack.js
@@ -1,14 +1,26 @@
 const { execSync } = require('child_process')
 
+const WINDOWS_PLATFORM_ID = 'win32';
+const LINUX_PLATFORM_ID = 'linux';
+const electronTargetVersion = '16.0.9';
+const targetArch = 'x64';
+
+const buildInstallCmd = (platform) => [
+    'npm i @pdftron/pdfnet-node --runtime=electron',
+    `--target=${electronTargetVersion}`,
+    `--target_platform=${platform}`,
+    `--target_arch=${targetArch}`,
+].join(' ');
+
 exports.default = async function(context) {
   // This runs before each build, perhaps T&S can use this to swap the binaries?
   const {
     electronPlatformName,
   } = context;
-  if (electronPlatformName === 'win32') {
-    execSync('npm i @pdftron/pdfnet-node --runtime=electron --target=16.0.9 --target_platform=win32 --target_arch=x64');
-  } else if (electronPlatformName === 'linux') {
-    execSync('npm i @pdftron/pdfnet-node --runtime=electron --target=16.0.9 --target_platform=linux --target_arch=x64');
+  if (electronPlatformName === WINDOWS_PLATFORM_ID) {
+    execSync(buildInstallCmd(WINDOWS_PLATFORM_ID));
+  } else if (electronPlatformName === LINUX_PLATFORM_ID) {
+    execSync(buildInstallCmd(LINUX_PLATFORM_ID));
   } else {
     console.log(`Unrecognized target: ${electronPlatformName}`)
   }
